cc293098ed41881e39dea3733626ba3c9b5867bb
anton.voskobovich <anton.voskobovich@gmail.com>
2015-04-04 20:09:39 +0300

    fully working game (no banners, no class inheritance)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var CELL_WIDTH = 101;
+var CELL_HEIGHT = 83;
+
+var Enemy = function(row, speedFactor) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = 0;
+    this.y = row*CELL_HEIGHT-20;
+    this.speedFactor = speedFactor || 10;
+    this.justAppeared = true;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +21,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x >= 5 * CELL_WIDTH || this.justAppeared) {
+        this.x = -1 * Math.random() * CELL_WIDTH;
+        this.speed = this.speedFactor * Math.random() * 5;
+        this.justAppeared = false;
+    }
+
+    this.x += this.speed*dt;
+    if(Math.abs(this.x - player.x) < 3*CELL_WIDTH/4 && Math.abs(this.y - player.y) < CELL_HEIGHT/2) {
+        console.log('Dead!');
+        player.init();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +42,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.init();
+}
+
+Player.prototype.init = function () {
+    this.x = 2*CELL_WIDTH;
+    this.y = 5*CELL_HEIGHT-10;
+}
+
+Player.prototype.update = function() {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    if(direction == 'left' && this.x > 0) {
+       this.x -= CELL_WIDTH;
+    } else if (direction == 'right' && this.x < 4*CELL_WIDTH) {
+       this.x += CELL_WIDTH;
+    } else if (direction == 'up' && this.y > 0) {
+       this.y -= CELL_HEIGHT;
+    } else if (direction == 'down' && this.y < 5*CELL_HEIGHT) {
+        this.y += CELL_HEIGHT;
+    }
+
+    if(this.y < 0) {
+       console.log('Won!');
+       this.init();
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+allEnemies = [new Enemy(1, 45), new Enemy(1, 10), new Enemy(2, 50), new Enemy(2, 100), new Enemy(3, 25)];
+player = new Player();
 
 
 // This listens for key presses and sends the keys to your
